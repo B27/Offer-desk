509ba8bc28c2d7c2e0f446b63c4d8b7510f1b288
B27 <ilia.burdukovski@ya.ru>
2019-10-22 17:51:09 +0800

    changed category model

--- src/controller/category.js
+++ src/controller/category.js
@@ -5,7 +5,11 @@ const jwt = require("jsonwebtoken");
 const [producer] = require("../emodels/producer");
 
 async function addCategory(ctx) {
-    const {} = ctx.request.body;
+    const { name } = ctx.request.body;
+    const [image] = ctx.request.files;
+    console.log("category name", name);
+
+    console.log("category image", image);
 }
 
 module.exports = {
