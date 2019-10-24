15c8710ebc7f5bc8b30e645c08c623e684812a37
B27 <ilia.burdukovski@ya.ru>
2019-10-24 14:56:58 +0800

    added save category into database

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,22 +1,23 @@
 const path = require("path");
 const os = require("os");
 const errorMessages = require("../../errorMessages");
-const constants = require("../../constants");
-
-const jwt = require("jsonwebtoken");
-const [producer] = require("../emodels/producer");
+const [Category] = require("../emodels/category");
 
 async function addCategory(ctx) {
-    const { name } = ctx.request.body;
-    const { image } = ctx.request.files;
-    console.log("category name", name);
+    const categoryName = ctx.request.body.name;
+    const image = ctx.request.files && ctx.request.files.image;
 
-    console.log("category image", image);
-    console.log(
-        "image.path basename",
-        os.platform() == "win32" ? path.win32.basename(image.path) : path.posix.basename(image.path)
-    );
+    ctx.assert(ctx.state.user.isAdmin, 403, errorMessages.mustBeTheAdmin());
 
+    const imageName =
+        image &&
+        (os.platform() === "win32"
+            ? path.win32.basename(image.path)
+            : path.posix.basename(image.path));
+
+    const saveCategoryResult = await Category.create({ name: categoryName, image: imageName });
+    saveCategoryResult.image = `${saveCategoryResult.image}`;
+    ctx.body = saveCategoryResult.toJSON({ user: ctx.state.user });
     ctx.status = 200;
 }
 
